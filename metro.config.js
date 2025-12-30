060d4ca4358a27335e9a2ea850b727ff768cf949
Yitao Chen <yitao@Yitaos-MacBook-Air-2.local>
2025-12-30 13:35:49 +0800

    Fix build: Remove problematic Metro blockList that was preventing compilation

--- metro.config.js
+++ metro.config.js
@@ -3,25 +3,18 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-// Add blockList to exclude problematic cache files
-config.resolver = {
-  ...config.resolver,
-  blockList: [
-    ...(config.resolver?.blockList || []),
-    /react-native-css-interop/,
-    /\.cache/,
-    /node_modules\/.*\.cache/,
-    /.*\.cache\/.*/,
-    /react-native-css-interop\/\.cache\/web\.css/
-  ]
-};
+// Temporarily disable blockList to see if build works
+// config.resolver = {
+//   ...config.resolver,
+//   blockList: [
+//     ...(config.resolver?.blockList || []),
+//     /react-native-css-interop\/\.cache/
+//   ]
+// };
 
-// Only apply NativeWind in development, skip in production build
-if (process.env.NODE_ENV !== 'production') {
-  module.exports = withNativeWind(config, {
-    input: "./global.css",
-    forceWriteFileSystem: true,
-  });
-} else {
-  module.exports = config;
-}
+module.exports = withNativeWind(config, {
+  input: "./global.css",
+  // Force write CSS to file system instead of virtual modules
+  // This fixes iOS styling issues in development mode
+  forceWriteFileSystem: true,
+});
